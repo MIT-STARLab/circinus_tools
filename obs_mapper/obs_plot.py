dabf5e298093f10e1570220a4008b5b7ea3e1f7d
Kit Kennedy <akennedy@mit.edu>
2018-04-05 19:40:53 -0400

    More small updates to the plotting tool, and added a couple specifications for observation targets in the tropics

--- obs_mapper/obs_plot.py
+++ obs_mapper/obs_plot.py
@@ -38,18 +38,28 @@ def grok_spec_rect(rect,item_uid,spec_option='solid'):
         ID = 0
         for lat_indx in range(num_lat):
             for lon_indx in range(num_lon):
-                lat = rect['lat_bottom_deg'] + lat_spacing*lat_indx
-                lon = rect['lon_left_deg'] + lon_spacing*lon_indx
+                # disclude this rectangle sub ID if specified
+                if ID in rect['disclude_points']:
+                    ID += 1
+                    continue
+
+                #  but each point in the middle of its bin
+                lat = rect['lat_bottom_deg'] + lat_spacing*lat_indx + lat_spacing/2
+                lon = rect['lon_left_deg'] + lon_spacing*lon_indx + lat_spacing/2
                 gpnts.append(GeoPoint(ID=ID, lat = lat,lon = lon))
                 ID += 1
 
     return grect,gpnts
 
 
-def grok_spec_pnt(pnt,item_uid,spec_option='default'):
+def grok_spec_pnt(pnt,item_uid):
+
+    spec_option = pnt['spec_option']
 
     if spec_option == 'default':
         return GeoPoint(ID = item_uid,lat = pnt['lat'],lon = pnt['lon'])
+    elif spec_option == 'ignore':
+        return None
     else:
         raise NotImplementedError
 
@@ -62,6 +72,8 @@ def plot_geo_point(plt,ax,gpnt):
     add_annotation(ax,gpnt.lat,gpnt.lon,str(gpnt.ID))
 
 def plot_rect(plt,ax,rect, item_uid):
+    num_points = 0
+
     spec_option = rect['spec_option']
 
     if spec_option == 'solid':
@@ -72,13 +84,25 @@ def plot_rect(plt,ax,rect, item_uid):
 
     elif spec_option == 'points':
         grect,gpnts = grok_spec_rect(rect,item_uid,spec_option)
-        add_annotation(ax,grect.lat-2,grect.lon-7,'r'+str(grect.ID))
 
         for gpnt in gpnts:
             plot_geo_point(plt, ax, gpnt)
 
+        num_points = len(gpnts)
+
+        if num_points > 0:
+            add_annotation(ax,grect.lat-2,grect.lon-7,'r'+str(grect.ID))
+        else:
+            print('Warning: no points found in %s'%(grect))
+    
+    elif spec_option == 'ignore':
+        pass
     else:
         raise NotImplementedError
+
+    return num_points
+
+
 
     
 
@@ -89,7 +113,7 @@ def plot_targets(targets_spec):
     plt.title('Observation target regions and points')
     
     fig = plt.gcf()
-    fig.set_size_inches(16,12)
+    fig.set_size_inches(20,16)
 
     plt.axis((-180, 180, -90, 90))
     plt.ylabel('Latitude')
@@ -105,27 +129,32 @@ def plot_targets(targets_spec):
     ax = plt.gca()    
     
     item_uid = 0
+    #  num points is different from number of IDs because some points can be discluded
+    num_points = 0 
 
     rect_items = targets_spec['rectangles']
     for rect in rect_items:
-        plot_rect(plt,ax,rect, item_uid)
+        num_points += plot_rect(plt,ax,rect, item_uid)
         item_uid += 1
 
 
     pnt_items = targets_spec['points']
     for point in pnt_items:
         gpnt = grok_spec_pnt(point,item_uid)
-        plot_geo_point(plt,ax,gpnt)
+        if gpnt:
+            plot_geo_point(plt,ax,gpnt)
+            num_points += 1
         item_uid += 1
     
     
+    print('num_points')
+    print(num_points)
 
-
-    # plt.show()
-    savefig('targets.pdf',format='pdf',bbox_inches='tight', transparent="True")
+    plt.show()
+    # savefig('targets.pdf',format='pdf',bbox_inches='tight', transparent="True")
 
 if __name__ == '__main__':
-    targets_spec_file = 'targets_spec/targets_1.json'
+    targets_spec_file = 'targets_spec/targets_tropics_land_loose.json'
 
     with open(targets_spec_file,'r') as f:
         targets_spec = json.load(f)
