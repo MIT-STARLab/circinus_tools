fa11fda4fcc2420d3aa1dfe12b354693867adcce
Kit Kennedy <akennedy@mit.edu>
2018-04-15 23:21:03 -0400

    helpful note to debug file

--- debug_tools.py
+++ debug_tools.py
@@ -2,6 +2,10 @@
 # ipdb> globals().update( locals() )
 #  got this from: https://github.com/inducer/pudb/issues/103
 
+# usage:
+# from circinus_tools import debug_tools
+# debug_tools.debug_breakpt()
+
 def debug_breakpt(option='ipdb'):
     if option == 'ipdb':
         print("YOU ARE IN debug_breakpt(). ENTER 's' TO GET TO DEBUG CALL")
