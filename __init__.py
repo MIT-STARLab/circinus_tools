9d11e4f093bb177b79aae0afc7bd2646f4ad2bfa
Kit Kennedy <akennedy@mit.edu>
2018-05-04 11:50:57 -0400

    restructuring a bit - shifting some utility files from GP repo over to here, packagicizing them

--- __init__.py
+++ __init__.py
@@ -1,6 +1,6 @@
 from . import time_tools
 from . import io_tools
 from . import constants
-from . import activity_window
 from . import debug_tools
 from . import matlab_if
+from .scheduling import activity_window,custom_activity_window,routing_objects,schedule_objects
---
+++ scheduling/__init__.py
@@ -0,0 +1,4 @@
+from . import activity_window
+from . import custom_activity_window
+from . import routing_objects
+from . import schedule_objects
