f032ce6be687b3c8b9e226d7ddf42cc0f456eed6
Kit Kennedy <akennedy@mit.edu>
2018-05-15 17:46:20 -0400

    Added import statement

--- __init__.py
+++ __init__.py
@@ -3,4 +3,5 @@ from . import io_tools
 from . import constants
 from . import debug_tools
 from . import matlab_if
+from . import sat_state_tools
 from .scheduling import base_window,custom_window,routing_objects,schedule_objects,io_processing
